45daad290ced3803c656f8d7b6a7f127090b2c02
shatunotv <48448955+shatunotv@users.noreply.github.com>
2019-04-08 12:25:24 +0300

    0204_2

--- src/ru/avalon/java/dev/j10/labs/Persones.java
+++ src/ru/avalon/java/dev/j10/labs/Persones.java
@@ -18,8 +18,8 @@ public class Persones implements Person {
  Date birthDate;
  
  
-  private static final SimpleDateFormat dateFormat 
-                   = new SimpleDateFormat("dd/MM/yyyy");
+  //private static final SimpleDateFormat dateFormat 
+     //              = new SimpleDateFormat("dd/MM/yyyy");
    
     String[] strings = {"9","2","3","5","6","7","8","1","4","10","12","11","13",
         "15","14","17","16","18","20","19"};
@@ -38,19 +38,19 @@ public Persones(String name, Date birthDate) {
         persons [2] = new Persones ("Миша", new Date((long)(21/07/1984)));         
         persons [3] = new Persones ("Вася", new Date((long)(12/01/2004)));         
         persons [4] = new Persones ("Коля", new Date((long)(22/12/1896)));         
-        persons [5] = new Persones ("Коля", new Date((long)(25/11/2002)));        
-        persons [6] = new Persones ("Коля", new Date((long)(8/04/2012))); 
-        persons [7] = new Persones ("Коля", new Date((long)(9/01/2014))); 
-        persons [8] = new Persones ("Коля", new Date((long)(01/02/2011))); 
+        persons [5] = new Persones ("Дима", new Date((long)(25/11/2002)));        
+        persons [6] = new Persones ("John", new Date((long)(8/04/2012))); 
+        persons [7] = new Persones ("Bill", new Date((long)(9/01/2014))); 
+        persons [8] = new Persones ("Donald", new Date((long)(01/02/2011))); 
         persons [9] = new Persones ("Коля", new Date((long)(03/02/2013))); 
         persons [1] = new Persones ("Коля", new Date((long)(07/9/1997)));      
         persons [2] = new Persones ("Миша", new Date((long)(05/02/2001)));         
         persons [3] = new Persones ("Вася", new Date((long)(06/8/2000)));       
-        persons [4] = new Persones ("Коля", new Date((long)(8/10/2001)));       
+        persons [4] = new Persones ("Jack", new Date((long)(8/10/2001)));       
         persons [5] = new Persones ("Коля", new Date((long)(11/02/2008)));        
-        persons [6] = new Persones ("Коля", new Date((long)(12/03/2006))); 
-        persons [7] = new Persones ("Коля", new Date((long)(15/05/2004))); 
-        persons [8] = new Persones ("Коля", new Date((long)(18/02/2003))); 
+        persons [6] = new Persones ("Витя", new Date((long)(12/03/2006))); 
+        persons [7] = new Persones ("Маша", new Date((long)(15/05/2004))); 
+        persons [8] = new Persones ("Даша", new Date((long)(18/02/2003))); 
         persons [9] = new Persones ("Коля", new Date((long)(15/04/1995))); 
         persons [10] = new Persones ("Коля", new Date((long)(01/02/1996))); 
 
@@ -61,7 +61,10 @@ public Persones(String name, Date birthDate) {
     }
     public Date getBirthDate() {
         return birthDate;
-   }             
+   }   
+
+    
+    
 }
 
 
